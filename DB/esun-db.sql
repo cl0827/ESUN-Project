16e535ce9df0f8f479d705c9be79eb8c503f4254
Louis <qazwsx50318@gmail.com>
2026-03-26 23:08:47 +0800

    修改DB

--- DB/esun-db.sql
+++ DB/esun-db.sql
@@ -1,1 +1,13 @@
-DROP TABLE esun_social_media.user;
+DELIMITER //
+
+CREATE PROCEDURE add_comment(
+    IN p_user_id INT,
+    IN p_post_id INT,
+    IN p_content TEXT
+)
+BEGIN
+    INSERT INTO comment (user_id, post_id, content, created_at)
+    VALUES (p_user_id, p_post_id, p_content, NOW());
+END //
+
+DELIMITER ;
